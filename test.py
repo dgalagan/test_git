61bf8bdcbfcf3bfa9a2562a24fd8d5a078c8f508
dgalagan <dmitriigalagan1991@gmail.com>
2026-02-18 15:28:08 +0100

    test_div1

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 print("hi")
 
-def add_num(num1, num2):
-    my_sum = num1 + num2
+def add_num(num1, num2, num3):
+    #change comment
+    my_sum = num1 + num2 + num3
     return my_sum
 
 def add_div(num1, num2):
